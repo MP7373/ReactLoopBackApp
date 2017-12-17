f6b00797f10eebb7412bc2918574247e7b3f4551
MP7373 <martinpayne73@gmail.com>
2017-12-16 20:13:56 -0500

    add delete and edit functionality

--- client_src/src/components/MeetupDetails.js
+++ client_src/src/components/MeetupDetails.js
@@ -25,18 +25,26 @@ class MeetupDetails extends Component {
       .catch(err => console.log(err));
   }
 
+  onDelete() {
+    let meetupId = this.state.details.id;
+    axios.delete(`http://localhost:3000/api/meetups/${meetupId}`)
+      .then(res => {
+        this.props.history.push('/');
+      }).catch(err => console.log(err));
+  }
+
   render() {
     return (
       <div>
-      <br />
+        <br />
         <Link className="btn grey" to="/">Back</Link>
         <h1>{this.state.details.name}</h1>
         <ul className="collection">
           <li className="collection-item">City: {this.state.details.city}</li>
           <li className="collection-item">Address: {this.state.details.address}</li>
         </ul>
-        <Link className="btn" to={`/meetups/edit/${this.state.details.id}`}>Edit</Link>
-        <button className="btn red right">Delete</button>
+        <Link className="btn" to={`./edit/${this.state.details.id}`}>Edit</Link>
+        <button onClick={this.onDelete.bind(this)}className="btn red right">Delete</button>
       </div>
     )
   }
